219eb3015210f6a4201efb25793fc901907715ad
kajan <kajan.mohanagandhirasa@proavos.com>
2017-08-08 15:39:57 +0530

    loose coupling data using resource bundle

--- src/main/java/com/k4j4n/controller/MinutesController.java
+++ src/main/java/com/k4j4n/controller/MinutesController.java
@@ -20,11 +20,13 @@ public class MinutesController {
 //        return "forward:addMoreMinutes.html";
         return "addMinutes";
 
-    }@RequestMapping(value = "/addMoreMinutes")
-    public String addMoreMinutes(@ModelAttribute ("exercise")Exercise exercise) {
-
-        System.out.println("exercising : " + exercise.getMinutes());
-
-        return "addMinutes";
     }
+
+//    @RequestMapping(value = "/addMoreMinutes")
+//    public String addMoreMinutes(@ModelAttribute ("exercise")Exercise exercise) {
+//
+//        System.out.println("exercising : " + exercise.getMinutes());
+//
+//        return "addMinutes";
+//    }
 }
--- target/FitnessTracker/WEB-INF/jsp/addMinutes.jsp
+++ target/FitnessTracker/WEB-INF/jsp/addMinutes.jsp
@@ -7,6 +7,8 @@
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+<%@ taglib prefix="sprig" uri="http://www.springframework.org/tags" %>
 <html>
 <head>
     <title>Add Minutes Page</title>
@@ -16,7 +18,7 @@
 <form:form commandName="exercise">
     <table>
         <tr>
-            <td>Minutes Exercise For Today</td>
+            <td><sprig:message code="goal.text"/> </td>
             <td><form:input path="minutes"/></td>
         </tr>
         <tr>
