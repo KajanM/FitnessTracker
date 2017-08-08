827b47815ce7737993dd47ac4c73e08b428ed151
kajan <kajan.mohanagandhirasa@proavos.com>
2017-08-08 15:11:05 +0530

    chained addMoreMinutes uri with addMinutes uri

--- src/main/java/com/k4j4n/controller/MinutesController.java
+++ src/main/java/com/k4j4n/controller/MinutesController.java
@@ -17,6 +17,14 @@ public class MinutesController {
 
         System.out.println("exercise : " + exercise.getMinutes());
 
+//        return "forward:addMoreMinutes.html";
+        return "redirect:addMoreMinutes.html";
+
+    }@RequestMapping(value = "/addMoreMinutes")
+    public String addMoreMinutes(@ModelAttribute ("exercise")Exercise exercise) {
+
+        System.out.println("exercising : " + exercise.getMinutes());
+
         return "addMinutes";
     }
 }
